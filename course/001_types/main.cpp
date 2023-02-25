502d1e00b8a022a360972f25b0acb0503a1a2ada
Grayni <Omenbestg@gmail.com>
2023-02-25 03:59:01 +0300

    types of variables

---
+++ course/001_types/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// dype_data name_variable
+void main()
+{
+	int a;
+	a = 42;
+
+	int Age = 31;
+	int Weight = 81;
+	int Height = 175;
+
+	double pi = 3.14;
+	char f = 'lf';
+	string k = "test string";
+	bool B = true;
+	bool C = 0;
+	
+
+	cout << a << endl;
+	cout << Age << " " << Weight << " " << Height << endl;
+	cout << pi << endl;
+	cout << k << endl;
+	cout << f << endl;
+	cout << B << endl;
+	cout << C << endl;
+}
