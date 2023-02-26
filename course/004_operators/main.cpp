a5eac659848075c7db6e7f0cf7e5be8b6933da7f
Grayni <Omenbestg@gmail.com>
2023-02-27 02:32:12 +0300

    operators

---
+++ course/004_operators/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int a = 5;
+	cout << a << endl;
+
+	a = -a;
+	cout << a << endl;
+
+	int b = 2;
+	int c;
+
+	c = a + b;
+	cout << c << endl;
+	cout << a * b << endl;
+	cout << a % c << endl;
+
+	// home
+	int l = 4;
+	int m = 2;
+	int n = 5;
+
+	cout << l + m + n << endl;
+	cout << l * m * n << endl;
+	cout << (double)(l + m + n) / 3 << endl;
+
+}
