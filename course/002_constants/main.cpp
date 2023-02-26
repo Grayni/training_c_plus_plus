d7181ef7b5a4ea9f9d1f3710619e953cfafc2896
Grayni <Omenbestg@gmail.com>
2023-02-26 11:30:13 +0300

    declaration of a constant variable

---
+++ course/002_constants/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	const int COUNT_WEEKDAYS = 7;
+	const char NEW_LINE = '\n';
+	const char TAB = '\t';
+
+	// error => COUNT_WEEKDAYS = 5;
+	
+	cout << COUNT_WEEKDAYS << NEW_LINE;
+	cout << COUNT_WEEKDAYS << TAB << TAB << COUNT_WEEKDAYS;
+
+}
