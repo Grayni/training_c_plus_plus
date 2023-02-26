238898f77c6951849ea15393b1ffae2569b572ad
Grayni <Omenbestg@gmail.com>
2023-02-26 11:46:35 +0300

    cin

---
+++ course/003_data_input/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int Var1;
+	int Var2;
+
+	cout << "Enter a number 1: ";
+	cin >> Var1;
+
+	cout << "Enter a nuber 2: ";
+	cin >> Var2;
+
+	cout << "Product of these numbers: " << Var1 * Var2 << endl;
+
+	cout << "Enter two numbers: ";
+	cin >> Var1 >> Var2;
+	cout << "Product of these nubers: " << Var1 * Var2 << endl;
+}
