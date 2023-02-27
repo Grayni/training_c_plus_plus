2d1ef8ab34ee8c02ca8c4b074cf1dc1be1823cbd
Grayni <Omenbestg@gmail.com>
2023-02-27 22:16:27 +0300

    increment, decrement, postfix, prefix, <fix indent>

--- course/005_increment_decrement/main.cpp
+++ course/005_increment_decrement/main.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 void main() {
-	int a = 1;
+    int a = 1;
 
-	a = a + 1;
-	cout << a << endl;
+    a = a + 1;
+    cout << a << endl;
 
-	a++;
-	cout << a << endl;
-
-
-	++a;
-	cout << a << endl;
-
-	--a;
-	cout << a << endl;
-
-	a--;
-	cout << a << endl;
+    a++;
+    cout << a << endl;
 
 
-	// difference b++ & ++b
-	int b = 1;
+    ++a;
+    cout << a << endl;
 
-	cout << endl << "Output b=1: " << b << endl;
-	cout << "Prefix add value now ++b: " << ++b << endl;
-	cout << "Postfix add value after implementation b++: " << b++ << endl;
-	cout << "Show b again. Now b change +1: " << b;
+    --a;
+    cout << a << endl;
+
+    a--;
+    cout << a << endl;
+
+
+    // difference b++ & ++b
+    int b = 1;
+
+    cout << endl << "Output b=1: " << b << endl;
+    cout << "Prefix add value now ++b: " << ++b << endl;
+    cout << "Postfix add value after implementation b++: " << b++ << endl;
+    cout << "Show b again. Now b change +1: " << b;
 
 
 }
