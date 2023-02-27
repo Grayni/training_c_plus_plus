8a08301f94f52ed059c2ac61b79d7c2169a38095
Grayni <Omenbestg@gmail.com>
2023-02-27 22:19:57 +0300

    ==, <=, >=, <, >, &&, ||, <fix indent>

--- course/007_logical_operations/main.cpp
+++ course/007_logical_operations/main.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 void main() {
-	cout << (4 > 5) << endl;  // 0
-	cout << (4 < 5) << endl;  // 1
+    cout << (4 > 5) << endl;  // 0
+    cout << (4 < 5) << endl;  // 1
 
-	int a = 1;
-	int b = 2;
+    int a = 1;
+    int b = 2;
 
-	cout << (a == b) << endl;
-	cout << (++a == b) << endl;
+    cout << (a == b) << endl;
+    cout << (++a == b) << endl;
 
-	cout << ((a == b) && (a == 3)) << endl;
-	cout << ((a == b) && (a == 2)) << endl;
+    cout << ((a == b) && (a == 3)) << endl;
+    cout << ((a == b) && (a == 2)) << endl;
 
-	cout << ((a == b) || (a == 3)) << endl;
-	cout << ((a != b) || (a == 3)) << endl;
+    cout << ((a == b) || (a == 3)) << endl;
+    cout << ((a != b) || (a == 3)) << endl;
 }
