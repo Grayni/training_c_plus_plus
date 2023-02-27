480bc70da4440f1ae18362d57577359f509996c8
Grayni <Omenbestg@gmail.com>
2023-02-27 22:15:13 +0300

    +, *, -, /, %, <fix indent>

--- course/004_operators/main.cpp
+++ course/004_operators/main.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 
 void main() {
-	int a = 5;
-	cout << a << endl;
+    int a = 5;
+    cout << a << endl;
 
-	a = -a;
-	cout << a << endl;
+    a = -a;
+    cout << a << endl;
 
-	int b = 2;
-	int c;
+    int b = 2;
+    int c;
 
-	c = a + b;
-	cout << c << endl;
-	cout << a * b << endl;
-	cout << a % c << endl;
+    c = a + b;
+    cout << c << endl;
+    cout << a * b << endl;
+    cout << a % c << endl;
 
-	// home
-	int l;
-	int m;
-	int n;
-	cout << "Enter 3 numbers: " << endl;
-	cin >> l >> m >> n;
+    // home
+    int l;
+    int m;
+    int n;
+    cout << "Enter 3 numbers: " << endl;
+    cin >> l >> m >> n;
 
-	cout << "Sum: " << l + m + n << endl;
-	cout << "Product: " << l * m * n << endl;
-	cout << "Mean: " << (double)(l + m + n) / 3 << endl;
+    cout << "Sum: " << l + m + n << endl;
+    cout << "Product: " << l * m * n << endl;
+    cout << "Mean: " << (double)(l + m + n) / 3 << endl;
 
 }
