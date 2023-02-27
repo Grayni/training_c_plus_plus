9bccb64540f1ceb469fd5bf0b954d7a978cdc9f7
Grayni <Omenbestg@gmail.com>
2023-02-27 22:18:47 +0300

    arithmetic forms, +=, -=, /=, <fix indent>

--- course/006_arithmetic_forms/main.cpp
+++ course/006_arithmetic_forms/main.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 void main() {
-	int a = 1;
+    int a = 1;
 
-	a = a + 1;
-	cout << a << endl;
+    a = a + 1;
+    cout << a << endl;
 
-	a += 1;
-	cout << a << endl;
+    a += 1;
+    cout << a << endl;
 
-	a -= 20;
-	cout << a << endl;
+    a -= 20;
+    cout << a << endl;
 
-	a /= 2;
-	cout << a << endl;
+    a /= 2;
+    cout << a << endl;
 }
