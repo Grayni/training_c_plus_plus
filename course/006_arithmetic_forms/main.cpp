e2eeef688c42f45fbfc502eddbca67789d03bbdb
Grayni <Omenbestg@gmail.com>
2023-02-27 11:29:34 +0300

    arithmetic forms

---
+++ course/006_arithmetic_forms/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int a = 1;
+
+	a = a + 1;
+	cout << a << endl;
+
+	a += 1;
+	cout << a << endl;
+
+	a -= 20;
+	cout << a << endl;
+
+	a /= 2;
+	cout << a << endl;
+}
