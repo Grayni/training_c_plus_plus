8bfa3f1a16a34e5abf31ad86b93b908d79b99b8a
Grayni <Omenbestg@gmail.com>
2023-02-27 21:58:02 +0300

    if, else if, else, &&, ||, <3

--- course/008_conditional/main.cpp
+++ course/008_conditional/main.cpp
@@ -2,44 +2,44 @@
 using namespace std;
 
 void main() {
-	bool isRain = true;
+    bool isRain = true;
 
-	if (isRain) {
-		cout << "need umbrella" << endl;
-	}
-	else {
-		cout << "good weather" << endl;
-	}
+    if (isRain) {
+        cout << "need umbrella" << endl;
+    }
+    else {
+        cout << "good weather" << endl;
+    }
 
-	// e.g. 1
-	int number;
+    // e.g. 1
+    int number;
 
-	cout << "Enter number from 1 to 20: ";
-	cin >> number;
-	
-	if (number > 15) {
-		cout << "This number > 15";
-	}
-	else if (number == 5) {
-		cout << "number = 5" << endl;
-	}
-	else if (number > 10) {
-		cout << "This number > 10" << endl;
-	}
-	else {
-		cout << "This number < 10" << endl;
-	}
+    cout << "Enter number from 1 to 20: ";
+    cin >> number;
 
-	// e.g. 2
-	int int_number;
+    if (number > 15) {
+        cout << "This number > 15";
+    }
+    else if (number == 5) {
+        cout << "number = 5" << endl;
+    }
+    else if (number > 10) {
+        cout << "This number > 10" << endl;
+    }
+    else {
+        cout << "This number < 10" << endl;
+    }
 
-	cout << endl << "Enter number: ";
-	cin >> int_number;
+    // e.g. 2
+    int int_number;
 
-	if (int_number % 2 == 0) {
-		cout << int_number << " - Even" << endl;
-	}
-	else {
-		cout << int_number << " - Odd" << endl;
-	}
+    cout << endl << "Enter number: ";
+    cin >> int_number;
+
+    if (int_number % 2 == 0) {
+        cout << int_number << " - Even" << endl;
+    }
+    else {
+        cout << int_number << " - Odd" << endl;
+    }
 }
