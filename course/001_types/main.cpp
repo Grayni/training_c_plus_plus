4ccb5a4f0af549811d82e74c0ff88a67e0d06436
Grayni <Omenbestg@gmail.com>
2023-02-27 22:10:34 +0300

    int, double, char, string, bool <fix indent>

--- course/001_types/main.cpp
+++ course/001_types/main.cpp
@@ -2,27 +2,26 @@
 using namespace std;
 
 // dype_data name_variable
-void main()
-{
-	int a;
-	a = 42;
+void main() {
+    int a;
+    a = 42;
 
-	int Age = 31;
-	int Weight = 81;
-	int Height = 175;
+    int Age = 31;
+    int Weight = 81;
+    int Height = 175;
 
-	double pi = 3.14;
-	char f = 'lf';
-	string k = "test string";
-	bool B = true;
-	bool C = 0;
-	
+    double pi = 3.14;
+    char f = 'lf';
+    string k = "test string";
+    bool B = true;
+    bool C = 0;
 
-	cout << a << endl;
-	cout << Age << " " << Weight << " " << Height << endl;
-	cout << pi << endl;
-	cout << k << endl;
-	cout << f << endl;
-	cout << B << endl;
-	cout << C << endl;
+
+    cout << a << endl;
+    cout << Age << " " << Weight << " " << Height << endl;
+    cout << pi << endl;
+    cout << k << endl;
+    cout << f << endl;
+    cout << B << endl;
+    cout << C << endl;
 }
