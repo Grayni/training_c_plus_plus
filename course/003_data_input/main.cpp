aefdaaea9b585270458765989651578199caa644
Grayni <Omenbestg@gmail.com>
2023-02-27 22:13:30 +0300

    declaration of a constant variable <fix indent>

--- course/003_data_input/main.cpp
+++ course/003_data_input/main.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 void main() {
-	int Var1;
-	int Var2;
+    int Var1;
+    int Var2;
 
-	cout << "Enter a number 1: ";
-	cin >> Var1;
+    cout << "Enter a number 1: ";
+    cin >> Var1;
 
-	cout << "Enter a nuber 2: ";
-	cin >> Var2;
+    cout << "Enter a nuber 2: ";
+    cin >> Var2;
 
-	cout << "Product of these numbers: " << Var1 * Var2 << endl;
+    cout << "Product of these numbers: " << Var1 * Var2 << endl;
 
-	cout << "Enter two numbers: ";
-	cin >> Var1 >> Var2;
-	cout << "Product of these nubers: " << Var1 * Var2 << endl;
+    cout << "Enter two numbers: ";
+    cin >> Var1 >> Var2;
+    cout << "Product of these nubers: " << Var1 * Var2 << endl;
 }
