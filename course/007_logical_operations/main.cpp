81c66a377f13e1492036ab8732025f1878cbef27
Grayni <Omenbestg@gmail.com>
2023-02-27 11:39:33 +0300

    ==, <=, >=, <, >, &&, ||

---
+++ course/007_logical_operations/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	cout << (4 > 5) << endl;  // 0
+	cout << (4 < 5) << endl;  // 1
+
+	int a = 1;
+	int b = 2;
+
+	cout << (a == b) << endl;
+	cout << (++a == b) << endl;
+
+	cout << ((a == b) && (a == 3)) << endl;
+	cout << ((a == b) && (a == 2)) << endl;
+
+	cout << ((a == b) || (a == 3)) << endl;
+	cout << ((a != b) || (a == 3)) << endl;
+}
