4f92749f98e49ec4589379511133f8a64a6c0b1b
Grayni <Omenbestg@gmail.com>
2023-02-28 11:06:57 +0300

    while

---
+++ course/009_while/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 0;
+
+    while (a < 10) {
+        cout << " (" << a << ") The display this line" << endl;
+        a++;
+    }
+}
