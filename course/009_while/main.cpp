6d351b8190ddf5d96d81d96426df9a32a902084e
Grayni <Omenbestg@gmail.com>
2023-02-28 11:22:57 +0300

    while, do while

--- course/009_while/main.cpp
+++ course/009_while/main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 int main() {
+    cout << " << cycle while >>" << endl << endl;
     int a = 0;
 
+    // liftoff
     while (a < 10) {
-        cout << " (" << a << ") The display this line" << endl;
-        a++;
+        cout << " (" << a++ << ") The display this line" << endl;
     }
+
+
+    a = 10;
+
+    // not liftoff
+    while (a < 10) {
+        cout << " (" << a++ << ") The display this line" << endl;
+    }
+
+
+    cout << endl << " << cycle do while >>" << endl << endl;
+    // liftoff
+    do {
+        cout << " (" << a++ << ") The display this line" << endl;
+    } while (a < 10);
 }
