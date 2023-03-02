d1eff0efee1cedec3f08d55f397aa157b816c3b2
Grayni <Omenbestg@gmail.com>
2023-03-02 22:35:33 +0300

    array & index > length array

---
+++ course/019_one-dimensional_array/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4];
+
+    arr[0] = 5;
+    arr[1] = 7;
+    arr[2] = 10;
+    arr[3] = 3333;
+
+    cout << arr[10] << endl;
+}
