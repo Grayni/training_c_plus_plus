08953445e484d30ae04635d9285e460a950e0d1d
Grayni <Omenbestg@gmail.com>
2023-03-18 23:27:14 +0300

    copy array

--- course/043_constant_arguments_function/main.cpp
+++ course/043_constant_arguments_function/main.cpp
@@ -8,17 +8,45 @@ void fillArray(int* const arr, const int size) {
     }
 }
 
-void showArray(int* const arr, const int size) {
-
+void showArray(const int* const arr, const int size, string name_arr) {
+    cout << name_arr << ": ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
+    cout << "\n";
 }
 
 void main() {
     const int size = 10;
-    int arr[size];
+    int array0[size];
 
-    fillArray(arr, size);
-    showArray(arr, size);
+    fillArray(array0, size);
+    showArray(array0, size, "array0");
+
+    int* array1 = new int[size];
+    int* array2 = new int[size];
+
+    fillArray(array1, size);
+    showArray(array1, size, "array1");
+
+    fillArray(array2, size);
+    showArray(array2, size, "array2");
+
+    /// copy array2
+    delete[] array1;
+
+    array1 = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        array1[i] = array2[i];
+    }
+
+    cout << endl;
+    showArray(array1, size, "array1");
+    showArray(array2, size, "array2");
+
+    delete[] array1;
+    delete[] array2;
+
+
 }
