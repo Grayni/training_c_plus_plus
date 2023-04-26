9e7a2674c632d31e721c093512b3877948a546ed
Grayni <Omenbestg@gmail.com>
2023-04-27 00:09:03 +0300

    multiple catch blocks

--- course/104_catch/main.cpp
+++ course/104_catch/main.cpp
@@ -4,8 +4,38 @@
 
 using namespace std;
 
+void Foo(int value) {
+
+    if (value < 0) {
+        //throw exception("invalid value: value < 0");
+        throw "invalid value: value < 0";
+    }
+
+    if (value == 0) {
+        throw exception("value == 0");
+    }
+
+    if (value == 1) {
+        throw 1;
+    }
+
+    cout << "variable = " << value << endl;
+
+}
+
 int main() {
-    //pass
+    try {
+        Foo(1);
+    }
+    catch (const exception& ex) {
+        cout << "block 1" << endl;
+    }
+    catch (const char* ex) {
+        cout << "block 2" << endl;
+    }
+    catch (...) {
+        cout << "any error" << endl;
+    }
 
     return 0;
 }
